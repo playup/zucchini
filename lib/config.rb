0f6cb3a18c747b2701c36574946e66a3cad3d944
Vasily Mikhaylichenko <vasily.mikhaylichenko@gmail.com>
2012-12-03 00:59:35 +1100

    Raise when running in Simulator and app isn't found

--- lib/config.rb
+++ lib/config.rb
@@ -1,29 +1,35 @@
 require 'yaml'
 
 module Zucchini
-  class Config 
-    
+  class Config
+
     def self.base_path
       @@base_path
     end
-  
+
     def self.base_path=(base_path)
       @@base_path = base_path
       @@config    = YAML::load_file("#{base_path}/support/config.yml")
     end
-                  
+
     def self.app
-      File.absolute_path( devices[ENV['ZUCCHINI_DEVICE']]['app'] || @@config['app'] )
+      device_name = ENV['ZUCCHINI_DEVICE']
+      app_path    = File.absolute_path(devices[device_name]['app'] || @@config['app'])
+
+      if device_name == 'iOS Simulator' && !File.exists?(app_path)
+        raise "Can't find application at path #{app_path}"
+      end
+      app_path
     end
-  
-    def self.resolution_name(dimension)   
+
+    def self.resolution_name(dimension)
       @@config['resolutions'][dimension.to_i]
     end
-  
+
     def self.devices
       @@config['devices']
     end
-  
+
     def self.device(device_name)
       raise "Device not listed in config.yml" unless (device = devices[device_name])
       {
@@ -32,16 +38,16 @@ module Zucchini
         :screen => device['screen']
       }
     end
-    
+
     def self.server(server_name)
       @@config['servers'][server_name]
     end
-  
+
     def self.url(server_name, href="")
       server_config = server(server_name)
       port = server_config['port'] ? ":#{server_config['port']}" : ""
-    
+
       "http://#{server_config['host']}#{port}#{href}"
     end
-  end                                   
+  end
 end
